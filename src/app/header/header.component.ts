2487f9f3187eae2fe5ff1b3eb3c5729217dc3c97
Lothar Zok <111051858+LotharZok@users.noreply.github.com>
2023-08-01 11:20:10 +0200

    added menu for responsive design

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+    @ViewChild('menuMobileOverlay') menuMobileOverlay!: ElementRef;
 
+    openMenu() {
+        console.log('openMenu gestartet');
+
+        let menuMobileOverlay = this.menuMobileOverlay.nativeElement;
+        menuMobileOverlay.classList.remove('d-none');
+    }
+
+    closeMenu() {
+        console.log('closeMenu gestartet');
+
+        let menuMobileOverlay = this.menuMobileOverlay.nativeElement;
+        menuMobileOverlay.classList.add('d-none');
+    }
 }
